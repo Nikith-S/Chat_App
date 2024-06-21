012a7e68fe39f85b3ec7857817d85b596da41e6a
Nikith-S <nikith391@gmail.com>
2024-06-22 00:33:14 +0530

    deployment

--- frontend/src/Hooks/useListenMessages.js
+++ frontend/src/Hooks/useListenMessages.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
 
-// import notificationSound from "../assets/sounds/notification.mp3";
+import notificationSound from "../assets/sounds/notification.mp3";
 
 const useListenMessages = () => {
 	const { socket } = useSocketContext();
@@ -11,9 +11,9 @@ const useListenMessages = () => {
 
 	useEffect(() => {
 		socket?.on("newMessage", (newMessage) => {
-			// newMessage.shouldShake = true;
-			// const sound = new Audio(notificationSound);
-			// sound.play();
+			newMessage.shouldShake = true;
+			const sound = new Audio(notificationSound);
+			sound.play();
 			setMessages([...messages, newMessage]);
 		});
 
